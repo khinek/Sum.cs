f6f626cfd129095b315b32020097f3f31df1725f
khinek\ <tom.shweko@gmail.com>
2014-09-18 08:39:45 -0400

    Start
    start

--- Program.cs
+++ Program.cs
@@ -9,7 +9,17 @@ namespace Sum.cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter any integer number from 
+            Console.WriteLine("Enter any integer number from 0 to 999");
+            int total = 0;
+            while (true)
+            {
+                int I = Convert.ToInt32(Console.ReadLine());
+                total += I;
+                Console.WriteLine("The total is " + total);
+                if (total >= 999)
+                    break;
+            }
+                
         }
     }
 }
